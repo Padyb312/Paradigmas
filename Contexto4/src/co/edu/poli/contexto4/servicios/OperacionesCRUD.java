0429f09101a3374462ce5c045c526747f9246f52
general <general@PGCITY42725-306.poligran.edu.co>
2026-03-20 15:49:47 -0500

    interface

---
+++ Contexto4/src/co/edu/poli/contexto4/servicios/OperacionesCRUD.java
@@ -0,0 +1,13 @@
+package co.edu.poli.contexto4.servicios;
+
+import java.util.Iterator;
+
+public interface OperacionesCRUD {
+
+	public String crear(TrajeHazmat traje) ;
+
+	public String leeruno(int indice) ;
+	
+
+	public String leertodos() ;
+}
